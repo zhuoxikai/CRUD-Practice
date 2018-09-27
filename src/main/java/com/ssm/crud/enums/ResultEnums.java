c1fe5bb16431ec843b33fd5cdb8f8b6c81f0a796
ljw9712 <ljw9712@163.com>
2018-09-28 01:53:39 +0800

    解决跨域的问题！

--- src/main/java/com/ssm/crud/enums/ResultEnums.java
+++ src/main/java/com/ssm/crud/enums/ResultEnums.java
@@ -1,9 +1,12 @@
 package com.ssm.crud.enums;
 
+import lombok.Getter;
+
 /**
  * @author Ernest
  * @date 2018/4/4 13:32
  */
+@Getter
 public enum ResultEnums {
     SUCCESS(100, "成功"),
     FAIL(200, "失败"),
@@ -15,14 +18,6 @@ public enum ResultEnums {
     ResultEnums(Integer code, String msg) {
         this.code = code;
         this.msg = msg;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public String getMsg() {
-        return msg;
     }
 }
 
